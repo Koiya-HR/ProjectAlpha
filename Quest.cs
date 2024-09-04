5eaeff4cf3518c9bdc1f34cfd27c772ac29c2ba9
1065800 <1065800@hr.nl>
2024-09-04 15:18:39 +0200

    Updated Quest.cs
    Added values and the constructor
    
    Hierbij weet ik niet meer hoe ik verder moet gaan

--- Quest.cs
+++ Quest.cs
@@ -1,9 +1,7 @@
 namespace ProjectAlpha;
 
-public class Quest {
-
-    public Quest() {
-
-    }
-
+public class Quest(int id, string name, string description) {
+    public int ID = id;
+    public string Name = name;
+    public string Description = description;
 }
