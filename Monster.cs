86b4d7ca7c3fb03601e20766fe6296074ca4e8c7
TF Rond <tijsrond@hotmail.nl>
2024-09-04 15:59:02 +0200

    ik heb de fields toegevoegd aan public class monster

--- Monster.cs
+++ Monster.cs
@@ -1,9 +1,16 @@
-namespace ProjectAlpha;
+namespace ProjectAlpha
+{
+    public class Monster
+    {
+        public int CurrentHitPoints;
+        public int ID;
+        public int MaximumDamage;
+        public int MaximumHitPoints;
+        public string Name;
 
-public class Monster {
+        public Monster()
+        {
 
-    public Monster() {
-
+        }
     }
-
 }
