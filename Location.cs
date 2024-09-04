3b42caef1637a14ee5cea8fcfe724b3f55f8c590
Koiya-HR <1041901@hr.nl>
2024-09-04 15:51:26 +0200

    Created base template for Location.cs class file.

--- Location.cs
+++ Location.cs
@@ -1,9 +1,27 @@
 namespace ProjectAlpha;
 
 public class Location {
+    public required int ID;
+    public required string Name;
+    public required string Description;
+    public required Quest QuestAvailableHere;
+    public required Monster MonsterLivingHere;
+    public required Location LocationToNorth;
+    public required Location LocationtoEast;
+    public required Location LocationtoSouth;
+    public required Location LocationtoWest;
 
-    public Location() {
-        
+
+    public Location(int id, string name, string description, Quest questavailablehere, Monster monsterlivinghere, Location locationtonorth, Location locationtoeast, Location locationtosouth, Location locationtowest) {
+        this.ID = id;
+        this.Name = name;
+        this.Description = description;
+        this.QuestAvailableHere = questavailablehere;
+        this.MonsterLivingHere = monsterlivinghere;
+        this.LocationToNorth = locationtonorth;
+        this.LocationtoEast = locationtoeast;
+        this.LocationtoSouth = locationtosouth;
+        this.LocationtoWest = locationtowest;
+
     }
-
 }
--- World.cs
+++ World.cs
@@ -1,3 +1,5 @@
+namespace ProjectAlpha;
+
 public static class World
 {
 
