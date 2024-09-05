01e8f86cfa6529537af2f3d3d0337c3922bdcdbd
Koiya-HR <1041901@hr.nl>
2024-09-05 10:19:19 +0200

    Finished setting up the Location.cs class. It now properly aligns with the expectations for the class in World.cs

--- Location.cs
+++ Location.cs
@@ -1,27 +1,22 @@
 namespace ProjectAlpha;
 
 public class Location {
-    public required int ID;
-    public required string Name;
-    public required string Description;
-    public required Quest QuestAvailableHere;
-    public required Monster MonsterLivingHere;
-    public required Location LocationToNorth;
-    public required Location LocationtoEast;
-    public required Location LocationtoSouth;
-    public required Location LocationtoWest;
+    public int ID;
+    public string Name;
+    public string Description;
+    public Quest? QuestAvailableHere;
+    public Monster? MonsterLivingHere;
+    public Location? LocationToNorth;
+    public Location? LocationToEast;
+    public Location? LocationToSouth;
+    public Location? LocationToWest;
 
 
-    public Location(int id, string name, string description, Quest questavailablehere, Monster monsterlivinghere, Location locationtonorth, Location locationtoeast, Location locationtosouth, Location locationtowest) {
+    public Location(int id, string name, string description, Quest? questavailablehere, Monster? monsterlivinghere) {
         this.ID = id;
         this.Name = name;
         this.Description = description;
         this.QuestAvailableHere = questavailablehere;
         this.MonsterLivingHere = monsterlivinghere;
-        this.LocationToNorth = locationtonorth;
-        this.LocationtoEast = locationtoeast;
-        this.LocationtoSouth = locationtosouth;
-        this.LocationtoWest = locationtowest;
-
     }
 }
