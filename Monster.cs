aeaf7468131b3fe157eb9c9151cece1ebb02225a
TF Rond <tijsrond@hotmail.nl>
2024-09-09 15:05:23 +0200

    Contructor gemaakt voor Monster class, begonnen met de implementatie van de attack functie.

--- Monster.cs
+++ Monster.cs
@@ -2,15 +2,40 @@ namespace ProjectAlpha
 {
     public class Monster
     {
-        public int CurrentHitPoints;
-        public int ID;
-        public int MaximumDamage;
-        public int MaximumHitPoints;
+        // field voor rng
+        Random rnd = new();
+
+        // fields voor monster properties
+        public int ID; 
         public string Name;
-
-        public Monster()
+        public int MaximumDamage;
+        public int CurrentHitPoints;
+        public int MaximumHitPoints;
+    
+        // constructor
+        public Monster(int id, string name, int maximumDamage, int currentHitPoints, int maximumHitPoints)
         {
+            ID = id;
+            Name = name;
+            MaximumDamage = maximumDamage;
+            CurrentHitPoints = currentHitPoints;
+            MaximumHitPoints = maximumHitPoints;
+        }
+        
+        // monster attack method
+        public void MonsterAttack(Player player)
+        {
+            int damage = rnd.Next(1, MaximumDamage + 1);
+            player.CurrentHitPoints -= damage;
 
+            if (player.CurrentHitPoints <= 0)
+            {
+                Console.WriteLine("The monster has defeated the player!");
+            }
+            else
+            {
+                Console.WriteLine($"The monster attacked the player for {damage} damage. The player's hit points are now {player.CurrentHitPoints}.");
+            }
         }
     }
 }
