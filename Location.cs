23bf4cf94e246007e425951f002c8b18b90dfe2c
Koiya-HR <1041901@hr.nl>
2024-09-09 19:18:04 +0200

    Updated a few small bugs/errors I overlooked in both the World.cs and the Location.cs file. Having updated these the 2 files should fit together flawlessly. XML documentation will be added in next Commit.

--- Location.cs
+++ Location.cs
@@ -15,15 +15,19 @@ public class Location {
     public int ID;
     public string Name;
     public string Description;
+    public Quest? QuestAvailableHere;
+    public Monster? MonsterLivingHere;
     public Location? LocationToNorth;
     public Location? LocationToEast;
     public Location? LocationToSouth;
     public Location? LocationToWest;
 
-    public Location(int id, string name, string description) {
+    public Location(int id, string name, string description, Quest? questavailablehere, Monster? monsterlivinghere ) {
         this.ID = id;
         this.Name = name;
         this.Description = description;
+        this.QuestAvailableHere = questavailablehere;
+        this.MonsterLivingHere = monsterlivinghere;
     }
 
     public void Map() {
