b36f6e39c241898e6d2a1a9aec76c76756166bf6
TF Rond <tijsrond@hotmail.nl>
2024-09-10 14:08:54 +0200

    bool toegevoegd om te checken om het monster nog leeft, hierbij heb ik ook nog een method voor de health van het monster en dat het monster damage kan krijgen

--- Monster.cs
+++ Monster.cs
@@ -6,12 +6,15 @@ namespace ProjectAlpha
         Random rnd = new();
 
         // fields voor monster properties
-        public int ID; 
+        public int ID;
         public string Name;
         public int MaximumDamage;
         public int CurrentHitPoints;
         public int MaximumHitPoints;
-    
+
+        // boolean om te checken of het monster nog leeft
+        public bool IsAlive;
+
         // constructor
         public Monster(int id, string name, int maximumDamage, int currentHitPoints, int maximumHitPoints)
         {
@@ -20,11 +23,18 @@ namespace ProjectAlpha
             MaximumDamage = maximumDamage;
             CurrentHitPoints = currentHitPoints;
             MaximumHitPoints = maximumHitPoints;
+            IsAlive = currentHitPoints > 0;
         }
-        
+
         // monster attack method
         public void MonsterAttack(Player player)
         {
+            if (!IsAlive)
+            {
+                Console.WriteLine("The monster is dead and cannot attack.");
+                return;
+            }
+
             int damage = rnd.Next(0, MaximumDamage + 1);
             player.CurrentHitPoints -= damage;
             if (damage == 0)
@@ -43,6 +53,39 @@ namespace ProjectAlpha
             else
             {
                 Console.WriteLine($"Player currently has {player.CurrentHitPoints} hit points remaining.");
+            }
+        }
+
+        public void MonsterHealth()
+        {
+            Console.WriteLine($"The monster has {CurrentHitPoints} hit points remaining.");
+            if (CurrentHitPoints <= 0)
+            {
+                IsAlive = false;
+                Console.WriteLine("The monster has been defeated.");
+            }
+        }
+
+        // Method om monster te kunnen attacken, deze versie is tijdelijk want er is nog geen player class
+        public void TakeDamage(int damage)
+        {
+            if (!IsAlive)
+            {
+                Console.WriteLine("The monster is already dead.");
+                return;
+            }
+
+            CurrentHitPoints -= damage;
+
+            if (CurrentHitPoints <= 0)
+            {
+                CurrentHitPoints = 0;
+                IsAlive = false;
+                Console.WriteLine("The monster has been defeated!.");
+            }
+            else
+            {
+                Console.WriteLine($"The monster took {damage} damage and now has {CurrentHitPoints} hit points remaining.");
             }
         }
     }
