2f2732d8c5a18aa6e3c200e1dd30c6ce49559dbc
TF Rond <tijsrond@hotmail.nl>
2024-09-18 14:13:50 +0200

    Update Monster.cs

--- Monster.cs
+++ Monster.cs
@@ -113,5 +113,32 @@ namespace ProjectAlpha
                 Console.WriteLine("The monster has been defeated.");
             }
         }
-    }
+
+        /// <summary>
+        /// Method to add a reward to the player's inventory upon defeating the monster
+        /// </summary>
+        /// <param name="player">The player who defeated the monster</param>
+        public void AddReward(Player player)
+        {
+            if (!IsAlive)
+            {
+                List<string> giveReward = new List<string> { "Health Potion" };
+
+                // Reward chance 50/50
+                if (rnd.Next(0, 2) == 1)
+                {
+                    string reward = giveReward[0];
+                    player.Inventory.Add(reward);
+                    Console.WriteLine($"You have received a {reward} for defeating the monster!");
+                }
+                else
+                {
+                    Console.WriteLine("The monster did not drop any items.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("The monster is still alive and cannot drop any items.");
+            }
+        }
 }
